83380e65f72f25e90f1fd074955ac5e3eb777360
BiBiOTA <yukiotataitien@gmail.com>
2021-12-20 00:03:46 +0800

    -- fixed -- input可輸入.

--- src/validator/validator.js
+++ src/validator/validator.js
@@ -18,14 +18,15 @@ export const checkIsIntegerGreaterThanZero = (v) => {
 @keycode(8) = BackSpace
 @keycode(38) = Up
 @keycode(40) = Down
+@keycode(190) = .
 */
 export const inputNumberDistinct = (e) => {
-  e.preventDefault();
   if(!((e.keyCode > 95 && e.keyCode < 106)
     || (e.keyCode > 47 && e.keyCode < 58) 
     || e.keyCode == 8
     || e.keyCode == 38
-    || e.keyCode == 40)) {
+    || e.keyCode == 40
+    || (e.keyCode == 190))) {
       e.target.value = '';
   }
 }
